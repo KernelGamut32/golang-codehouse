a84feb5f8c9de4f187e5834126aa2514b2ac7c9b
Allen Sanders <asanders@gamuttechnologysvcs.com>
2021-07-25 14:02:54 -0400

    Updates

--- exercises/gin/backend/cmd/main.go
+++ exercises/gin/backend/cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/KernelGamut32/golang-codehouse/exercises/gin/backend/internal/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	routes.SetupRoutes(r)
 	r.Run()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/KernelGamut32/golang-codehouse
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.7.2
+)
